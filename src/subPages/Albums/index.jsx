3d9a5d6b190e4456e7a674725882c0499f522469
Rodrigo Oliveira <rodrigooliveiramattos2006@gmail.com>
2024-03-24 16:20:14 -0400

    animação no slide dos albums

--- src/subPages/Albums/index.jsx
+++ src/subPages/Albums/index.jsx
@@ -1,4 +1,5 @@
 import "./styleAlbums.css"
+import "./albumAnimation.css"
 import { albums } from "../../util/json/albums/albums.json"
 import { useState } from "react";
 import { useEffect } from "react";
@@ -6,47 +7,88 @@ import MiniInfo from "../../components/MiniInfo"
 
 const Albums = () => {
     const [index, setIndex] = useState(0);
-    const [prevIndex, setPrevIndex] = useState(0);
-    const [nextIndex, setNextIndex] = useState(0);
+    const [prevIndex, setPrevIndex] = useState('start');
+    const [nextIndex, setNextIndex] = useState('start');
     const [miniInfo, setMiniInfo] = useState(albums[index]);
-    console.log(miniInfo);
-    
+
     useEffect(() => {
-        const next = document.querySelector(".album-swipe__img.next");
-        const active = document.querySelector(".album-swipe__img.active");
-        const prev = document.querySelector(".album-swipe__img.prev");
-        const other = document.querySelector(".album-swipe__img.other");
+        if (prevIndex !== 'start') {
+            const nextAnimation = document.querySelector(".album-swipe__img.next");
+            const activeAnimation = document.querySelector(".album-swipe__img.active");
+            const prevAnimation = document.querySelector(".album-swipe__img.prev");
+            const otherAnimation = document.querySelector(".album-swipe__img.other");
 
-        next.classList.remove("next");
-        active.classList.remove("active");
-        prev.classList.remove("prev");
-        other.classList.remove("other");
+            nextAnimation.classList.add('animationPrev');
+            activeAnimation.classList.add('animationPrev');
+            prevAnimation.classList.add('animationPrev');
+            otherAnimation.classList.add('animationPrev');
 
-        next.classList.add("other");
-        active.classList.add("next");
-        prev.classList.add("active");
-        other.classList.add("prev");
+            setTimeout(() => {
+                const next = document.querySelector(".album-swipe__img.next");
+                const active = document.querySelector(".album-swipe__img.active");
+                const prev = document.querySelector(".album-swipe__img.prev");
+                const other = document.querySelector(".album-swipe__img.other");
+
+                next.classList.remove("next");
+                active.classList.remove("active");
+                prev.classList.remove("prev");
+                other.classList.remove("other");
+
+                next.classList.add("other");
+                active.classList.add("next");
+                prev.classList.add("active");
+                other.classList.add("prev");
+
+                nextAnimation.classList.remove('animationPrev');
+                activeAnimation.classList.remove('animationPrev');
+                prevAnimation.classList.remove('animationPrev');
+                otherAnimation.classList.remove('animationPrev');
+            }, 500);
+        }
+
     }, [prevIndex])
 
     useEffect(() => {
-        const next = document.querySelector(".album-swipe__img.next");
-        const active = document.querySelector(".album-swipe__img.active");
-        const prev = document.querySelector(".album-swipe__img.prev");
-        const other = document.querySelector(".album-swipe__img.other");
+        if (nextIndex !== 'start') {
+            const nextAnimation = document.querySelector(".album-swipe__img.next");
+            const activeAnimation = document.querySelector(".album-swipe__img.active");
+            const prevAnimation = document.querySelector(".album-swipe__img.prev");
+            const otherAnimation = document.querySelector(".album-swipe__img.other");
 
-        next.classList.remove("next");
-        active.classList.remove("active");
-        prev.classList.remove("prev");
-        other.classList.remove("other");
+            nextAnimation.classList.add('animationNext');
+            activeAnimation.classList.add('animationNext');
+            prevAnimation.classList.add('animationNext');
+            otherAnimation.classList.add('animationNext');
 
-        next.classList.add("active");
-        active.classList.add("prev");
-        prev.classList.add("other");
-        other.classList.add("next");
+            setTimeout(() => {
+                const next = document.querySelector(".album-swipe__img.next");
+                const active = document.querySelector(".album-swipe__img.active");
+                const prev = document.querySelector(".album-swipe__img.prev");
+                const other = document.querySelector(".album-swipe__img.other");
+
+                next.classList.remove("next");
+                active.classList.remove("active");
+                prev.classList.remove("prev");
+                other.classList.remove("other");
+
+                next.classList.add("active");
+                active.classList.add("prev");
+                prev.classList.add("other");
+                other.classList.add("next");
+
+                nextAnimation.classList.remove('animationNext');
+                activeAnimation.classList.remove('animationNext');
+                prevAnimation.classList.remove('animationNext');
+                otherAnimation.classList.remove('animationNext');
+            }, 500);
+        }
+
     }, [nextIndex])
 
     useEffect(() => {
-        setMiniInfo(albums[index]);
+        setTimeout(() => {
+            setMiniInfo(albums[index]);
+        }, 500);
     }, [index])
 
     function handlePrevIndex() {
@@ -63,6 +105,8 @@ const Albums = () => {
             <div className="swipe-disc">
 
                 <div className="album-swipe">
+                    <button className="prevButton" onClick={() => handlePrevIndex()}></button>
+                    <button className="nextButton" onClick={() => handleNextIndex()}></button>
                     <div className="album-swipe__img active">
                         <img className="cd" src="/public/albums/cd.png" alt="cd" />
                         <img className="album-cover" src="/public/albums/2018-maenarawanai.jpg" alt="Wakage Ga Itaru" />
@@ -80,8 +124,7 @@ const Albums = () => {
                         <img className="album-cover" src="/public/albums/2023-ichijikikoku.jpg" alt="Wakage Ga Itaru" />
                     </div>
 
-                    <button className="prevButton" onClick={() => handlePrevIndex()}></button>
-                    <button className="nextButton" onClick={() => handleNextIndex()}></button>
+
                 </div>
                 <MiniInfo miniInfo={miniInfo} />
             </div>
