f9776f58651dc4ab0207d933be9b21d4717f600b
Rodrigo Oliveira <rodrigooliveiramattos2006@gmail.com>
2024-03-26 21:04:03 -0400

    header responsivo quase pronto

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import "./styleHeader.css"
+import "./styleHeaderHamburguer.css"
 import { useState, useEffect } from "react"
 
 function Header() {
@@ -7,6 +7,8 @@ function Header() {
     const [membersClass, setMembersClass] = useState("header-menu__sections")
     const [albunsClass, setAlbunsClass] = useState("header-menu__sections")
     const [musicClass, setMusicClass] = useState("header-menu__sections")
+    const setArray = [setHeroClass, setMembersClass, setAlbunsClass, setMusicClass]
+
     const heroHeight = 700
     const memberHeight = heroHeight + 800
     const albumHeight = memberHeight + 700
@@ -15,54 +17,52 @@ function Header() {
     window.onscroll = () => {
         position = window.pageYOffset
         if (position >= 0 && position <= heroHeight) {
-            setHeroClass("header-menu__sections active")
-            setMembersClass("header-menu__sections")
-            setAlbunsClass("header-menu__sections")
-            setMusicClass("header-menu__sections")
+            setArray.forEach((item) => item == setArray[0] ? item("header-menu__sections active") : item("header-menu__sections"))
         } else if (position > heroHeight && position <= memberHeight) {
-            setHeroClass("header-menu__sections")
-            setMembersClass("header-menu__sections active")
-            setAlbunsClass("header-menu__sections")
-            setMusicClass("header-menu__sections")
+            setArray.forEach((item) => item == setArray[1] ? item("header-menu__sections active") : item("header-menu__sections"))
         } else if (position > memberHeight && position <= albumHeight) {
-            setHeroClass("header-menu__sections")
-            setMembersClass("header-menu__sections")
-            setAlbunsClass("header-menu__sections active")
-            setMusicClass("header-menu__sections")
+            setArray.forEach((item) => item == setArray[2] ? item("header-menu__sections active") : item("header-menu__sections"))
         } else if (position > albumHeight) {
-            setHeroClass("header-menu__sections")
-            setMembersClass("header-menu__sections")
-            setAlbunsClass("header-menu__sections")
-            setMusicClass("header-menu__sections active")
+            setArray.forEach((item) => item == setArray[3] ? item("header-menu__sections active") : item("header-menu__sections"))
         }
     };
-    
+
     useEffect(() => {
-        let atualActive = document.querySelector(".active")
         const headerMenu = document.querySelectorAll(".header-menu__sections")
-        headerMenu.forEach(li => {
-            li.addEventListener("click", () => {
+        let atualActive = document.querySelector(".active")
+
+        headerMenu.forEach(section => {
+            section.addEventListener("click", () => {
                 atualActive.classList.remove("active")
-                atualActive = li
-                li.classList.add("active")
+                atualActive = section
+                section.classList.add("active")
             })
         });
     }, [])
 
     return (
         <HeaderMainPage className="header-main-page">
-            <div className="logo">
+            <Logo className="logo">
                 <a href="https://atarashiigakko.com">
-                    <img className="logo" src="/public/logo/AG-white.png" alt="LOGO ATARASHII GAKKO!" />
+                    <img src="/public/logo/AG-white.png" alt="LOGO ATARASHII GAKKO!" />
                 </a>
-            </div>
-            <nav>
-                <ul className="header-menu">
-                    <li className={heroClass}><a href="#hero">Atarashii Gakko!</a></li>
-                    <li className={membersClass}><a href="#members">Members</a></li>
-                    <li className={albunsClass}><a href="#albums">Albums</a></li>
-                    <li className={musicClass}><a href="#music">Music</a></li>
-                </ul>
+            </Logo>
+            <nav role="navigation">
+                <div id="menuToggle">
+                    <div className="hamburguer">
+                        <input type="checkbox" />
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+
+                    <UlMenu className="header-menu">
+                        <li className={heroClass}><a href="#hero">Atarashii Gakko!</a></li>
+                        <li className={membersClass}><a href="#members">Members</a></li>
+                        <li className={albunsClass}><a href="#albums">Albums</a></li>
+                        <li className={musicClass}><a href="#music">Music</a></li>
+                    </UlMenu>
+                </div>
             </nav>
 
         </HeaderMainPage>
@@ -70,8 +70,13 @@ function Header() {
 }
 
 const HeaderMainPage = styled.header`
-    background-color: var(--tema-claro-6);
-    gap: 6rem;
+    // background-color: var(--tema-claro-6);
+
+    background: rgba(57, 88, 134, 0.9);
+    box-shadow: 0 8px 32px 10px rgba(31, 38, 135, 0.37);
+    backdrop-filter: blur(3px);
+    -webkit-backdrop-filter: blur(3px);
+
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -80,11 +85,80 @@ const HeaderMainPage = styled.header`
     padding: 0 2rem;
     position: fixed;
     z-index: 999;
-    letter-spacing: 2px;
+
     nav {
-    flex: 1;
+        flex: 1;
+        height: 100%;
+    }
+`
+
+const Logo = styled.div`
+    height: 100%;
+    padding: 2rem 0;
+
+    img{
     height: 100%;
     }
+`
+
+const UlMenu = styled.ul`
+    flex: 1;
+    height: 100%;
+    display: flex;
+    justify-content: end;
+    align-items: center;
+    letter-spacing: 3px;
+
+    .header-menu__sections {
+        flex: 1;
+        display: grid;
+        place-items: center;
+        max-width: 15rem;
+        opacity: 0.4;
+        position: relative;
+        height: 100%;
+        cursor: pointer;
+
+        &::before {
+            content: "";
+            position: absolute;
+            bottom: 0;
+            width: 70%;
+            height: 7px;
+            background-color: var(--tema-claro-5);
+            opacity: 0.6;
+            border-radius: 10px 10px 0 0;
+        }
+
+        a {
+            text-align: center;
+            font-weight: bold;
+            text-transform: uppercase;
+            color: var(--tema-claro-1);
+            display: grid;
+            place-items: center;
+            font-size: 1.6em;
+            height: 10rem;
+            width: 100%;
+        }
+        &:hover {
+            opacity: 1;
+            &::before {
+                background-color: var(--tema-claro-4);
+                z-index: -1;
+            }
+        }
+        }
+        .header-menu__sections.active {
+            opacity: 0.8;
+            &:hover {
+                opacity: 1;
+            }
+            &::before {
+                opacity: 1;
+                background-color: var(--tema-claro-2);
+            }
+        }
 `
 
 export default Header
